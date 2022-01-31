0b36a854369a4703db2c54f42563e6b76ff0cdb5
REZA NIKFAL <mr.nikfal@gmail.com>
2022-01-31 13:05:02 -0600

    CommitMain

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -9,6 +9,26 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class TodoFormComponent {
   form = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('')
+    email: new FormControl(''),
+    email2: new FormControl(''),
+    email3: new FormControl(''),
+    email4: new FormControl(''),
+    email5: new FormControl('')
+  })
+  form2 = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl(''),
+    email2: new FormControl(''),
+    email3: new FormControl(''),
+    email4: new FormControl(''),
+    email5: new FormControl('')
+  })
+  form3 = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl(''),
+    email2: new FormControl(''),
+    email3: new FormControl(''),
+    email4: new FormControl(''),
+    email5: new FormControl('')
   })
 }
